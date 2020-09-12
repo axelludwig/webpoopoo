6140d829005bc7ca674f0b4294529d2e1a057167
Guillaume Riedinger <guikssriedinger@gmail.com>
2020-09-12 22:41:55 +0200

    Adding  server

---
+++ Server/WebPooPooServer/WebPooPooServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebPooPooServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
