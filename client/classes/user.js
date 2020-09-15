ccc59b83dc7e50fce0df4fa28300f3e9b282a85b
Axel LUDWIG <axell@sxb.incotec.fr>
2020-09-15 15:51:59 +0200

    add user and room classes

---
+++ client/classes/user.js
@@ -0,0 +1,10 @@
+class User {
+    constructor(username, id) {
+        this.username = username;
+        this.id = id;
+    }
+
+    updateUsername(newusername) {
+        this.username = newusername;
+    } 
+}
